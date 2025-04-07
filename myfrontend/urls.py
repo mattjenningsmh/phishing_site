5fbeb227c5aceb6ad0722893ee126dcb6d369823
matt jennings <mattjennings@MacBookAir.cougars.int>
2025-04-07 17:02:11 -0400

    uhhh

--- myfrontend/urls.py
+++ myfrontend/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='google'),
     path('input/', views.text_input_view, name='text_input'),
-    path('google/', views.google_view, name='google'),
+    
     path('sign-in/', views.sign_in_view, name='sign_in')
 ]
